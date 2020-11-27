d7c6fa0878bc9003345af533d7924258dc0bbbc8
maixuanlinh <maixuanlinhdkh@gmail.com>
2020-11-27 04:37:56 +0200

    Initial Commit

---
+++ Let's Chat - By MXL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Let's Chat - By MXL
+//
+//  Created by Mai Xuan Linh on 27.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
